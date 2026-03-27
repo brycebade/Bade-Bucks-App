4760b351a76284e942c532643ae385f6a928395c
brycebade <229314181+brycebade@users.noreply.github.com>
2026-03-27 06:09:44 -0500

    Implement pay display update logic
    Added pay display update based on completed chores count.

--- index.js
+++ index.js
@@ -1,20 +1,32 @@
 const dayChecks = document.querySelectorAll(".dayCheckbox")
-const choresCompleted = document.getElementById("choresCompleted")
+const choresDisplay = document.getElementById("choresCompleted")
+const payDisplay = document.getElementById("payDue")
 
+
+
+const updatePayDisplay = () => {
+    if (count === 1) {
+        payDisplay.textContent = `Pay Due: $25`
+    } else if (count === 2) {
+        payDisplay.textContent = `Pay Due: $50`
+    } else if (count === 3) {
+        payDisplay.textContent = `Pay Due: $75`
+    } else {
+        payDisplay.textContent = `Pay Due: `
+    }
+}
 
 const updateChoresCompleted = () => {
     let count = 0
-
     dayChecks.forEach((dayCheck) => {
         if (dayCheck.checked) {
             count++
         }
     })
-
-    console.log(count)
+    choresDisplay.textContent = `Chores Completed: ${count} / 3`
+    updatePayDisplay()   
 }
 
 dayChecks.forEach((dayCheck) => {
     dayCheck.addEventListener("change", updateChoresCompleted)
-    choresCompleted.textContent = updateChoresCompleted()
 })
