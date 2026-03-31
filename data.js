556b5332351444f49ffe975d5a10d5e523c4a691
brycebade <229314181+brycebade@users.noreply.github.com>
2026-03-31 05:42:12 -0500

    Add children data structure with chore rates

---
+++ data.js
@@ -0,0 +1,57 @@
+const children = [
+    {
+        id: 1,
+        name: "Haylee",
+        perChoreRate: 25,
+        weeks: [
+            {
+                weekStart: "2026-03-30",
+                monday: false,
+                tuesday: false,
+                wednesday: false,
+                thursday: false,
+                friday: false,
+                saturday: false,
+                sunday: false,
+                isPaid: false
+            }
+        ]
+    },
+    {
+        id: 2,
+        name: "Maxon",
+        perChoreRate: 32,
+        weeks: [
+            {
+                weekStart: "2026-03-30",
+                monday: false,
+                tuesday: false,
+                wednesday: false,
+                thursday: false,
+                friday: false,
+                saturday: false,
+                sunday: false,
+                isPaid: false
+            }
+        ]
+        
+    },
+    {
+        id: 3,
+        name: "Oliver",
+        perChoreRate: 25,
+        weeks: [
+            {
+                weekStart: "2026-03-30",
+                monday: false,
+                tuesday: false,
+                wednesday: false,
+                thursday: false,
+                friday: false,
+                saturday: false,
+                sunday: false,
+                isPaid: false
+            }
+        ]
+    }
+]
