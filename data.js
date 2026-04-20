c0bf33066c947fcbec50677afe51c9ef69f026ac
brycebade <229314181+brycebade@users.noreply.github.com>
2026-04-20 06:38:15 -0500

    Clear weeks data for all children
    Removed all week entries for each child and replaced with empty weeks array.

--- data.js
+++ data.js
@@ -1,6 +1,6 @@
 export const children = [
     {
-        id: 1,
+          id: 1,
         name: "Haylee",
         payRates: {
             0: 0,
@@ -12,52 +12,7 @@ export const children = [
             6: 150,
             7: 175
         },
-        weeks: [
-            {
-                weekStart: "2026-04-06",
-                monday: false,
-                tuesday: false,
-                wednesday: false,
-                thursday: false,
-                friday: false,
-                saturday: false,
-                sunday: false,
-                isPaid: false
-            },
-            {
-                weekStart: "2026-04-13",
-                monday: false,
-                tuesday: false,
-                wednesday: false,
-                thursday: false,
-                friday: false,
-                saturday: false,
-                sunday: false,
-                isPaid: false,
-            },
-            {
-                weekStart: "2026-04-20",
-                monday: false,
-                tuesday: false,
-                wednesday: false,
-                thursday: false,
-                friday: false,
-                saturday: false,
-                sunday: false,
-                isPaid: false,
-            },
-            {
-                weekStart: "2026-04-27",
-                monday: false,
-                tuesday: false,
-                wednesday: false,
-                thursday: false,
-                friday: false,
-                saturday: false,
-                sunday: false,
-                isPaid: false,
-            }
-        ]
+        weeks: []
     },
     {
         id: 2,
@@ -72,52 +27,7 @@ export const children = [
             6: 185,
             7: 215
         },
-        weeks: [
-            {
-                weekStart: "2026-04-06",
-                monday: false,
-                tuesday: false,
-                wednesday: false,
-                thursday: false,
-                friday: false,
-                saturday: false,
-                sunday: false,
-                isPaid: false
-            },
-            {
-                weekStart: "2026-04-13",
-                monday: false,
-                tuesday: false,
-                wednesday: false,
-                thursday: false,
-                friday: false,
-                saturday: false,
-                sunday: false,
-                isPaid: false,
-            },
-            {
-                weekStart: "2026-04-20",
-                monday: false,
-                tuesday: false,
-                wednesday: false,
-                thursday: false,
-                friday: false,
-                saturday: false,
-                sunday: false,
-                isPaid: false,
-            },
-            {
-                weekStart: "2026-04-27",
-                monday: false,
-                tuesday: false,
-                wednesday: false,
-                thursday: false,
-                friday: false,
-                saturday: false,
-                sunday: false,
-                isPaid: false,
-            }
-        ]
+        weeks: []
     },
     {
         id: 3,
@@ -132,51 +42,6 @@ export const children = [
             6: 150,
             7: 175
         },
-        weeks: [
-            {
-                weekStart: "2026-04-06",
-                monday: false,
-                tuesday: false,
-                wednesday: false,
-                thursday: false,
-                friday: false,
-                saturday: false,
-                sunday: false,
-                isPaid: false
-            },
-            {
-                weekStart: "2026-04-13",
-                monday: false,
-                tuesday: false,
-                wednesday: false,
-                thursday: false,
-                friday: false,
-                saturday: false,
-                sunday: false,
-                isPaid: false,
-            },
-            {
-                weekStart: "2026-04-20",
-                monday: false,
-                tuesday: false,
-                wednesday: false,
-                thursday: false,
-                friday: false,
-                saturday: false,
-                sunday: false,
-                isPaid: false,
-            },
-            {
-                weekStart: "2026-04-27",
-                monday: false,
-                tuesday: false,
-                wednesday: false,
-                thursday: false,
-                friday: false,
-                saturday: false,
-                sunday: false,
-                isPaid: false,
-            }
-        ]
+        weeks: []
     }
 ]
