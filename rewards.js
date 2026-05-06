9c759756465dcf1db7d963b3213eb0145bd22be8
brycebade <229314181+brycebade@users.noreply.github.com>
2026-05-06 19:32:41 +0000

    create reward.js

---
+++ rewards.js
@@ -0,0 +1,102 @@
+export const rewards = [
+    {
+        id: 1,
+        rewardItem: "30min Extra Electronics",
+        cost: 50 
+    },
+    {
+        id: 2,
+        rewardItem: "60min Extra Electronics",
+        cost: 100
+    },
+    {
+        id: 3,
+        rewardItem: "Screen Saturday",
+        cost: 300
+    },
+    {
+        id: 4,
+        rewardItem: "60min Living Room TV",
+        cost: 50
+    },
+    {
+        id: 5,
+        rewardItem: "Adult Menu",
+        cost: 350
+    },
+    {
+        id: 6,
+        rewardItem: "McDonald's Lunch",
+        cost: 200
+    },
+    {
+        id: 7,
+        rewardItem: "Hot Lunch",
+        cost: 100
+    },
+    {
+        id: 8,
+        rewardItem: "Bowl of Popcorn",
+        cost: 50
+    },
+    {
+        id: 9,
+        rewardItem: "Soda with Meal",
+        cost: 40
+    },
+    {
+        id: 10,
+        rewardItem: "Fountain at Gas Station",
+        cost: 40
+    },
+    {
+        id: 11,
+        rewardItem: "Paid Time Off",
+        cost: 200
+    },
+    {
+        id: 12,
+        rewardItem: "Stay Up 30min Later",
+        cost: 60
+    },
+    {
+        id: 13,
+        rewardItem: "Stay up 60min Later",
+        cost: 100
+    },
+    {
+        id: 14,
+        rewardItem: "Movie Night of Choice",
+        cost: 5
+    },
+    {
+        id: 15,
+        rewardItem: "Game Night of Choice",
+        cost: 5
+    },
+    {
+        id: 16,
+        rewardItem: "$5 Cash",
+        cost: 150
+    },
+    {
+        id: 17,
+        rewardItem: "$10 cash",
+        cost: 300
+    },
+    {
+        id: 18,
+        rewardItem: "$20 cash",
+        cost: 600
+    },
+    {
+        id: 19,
+        rewardItem: "$40 cash",
+        cost: 1200
+    },
+    {
+        id: 20,
+        rewardItem: "$50 cash",
+        cost: 1500
+    }
+]
