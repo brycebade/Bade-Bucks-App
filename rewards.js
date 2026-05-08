f6a97229ca4b0fa1c6083d51fad68f8944bbc8ef
brycebade <bryce.bade@gmail.com>
2026-05-08 05:23:51 -0500

    add classes to rewards.js to format the reward cards on rewards.html

--- rewards.js
+++ rewards.js
@@ -6,18 +6,18 @@ rewards.forEach((reward) => {
   const rewardCard = document.createElement("div")
   rewardCard.classList.add("card", "bg-base-100", "shadow-md", "overflow-hidden")
   rewardCard.innerHTML = `
-    <div class="card-body">
-      <h3 class="card-title">${reward.item}</h3>
+    <div class="card-body p-4 flex flex-col items-center">
+      <h3 class="card-title text-base text-center justify-center min-h-[60px]">${reward.item}</h3>
 
-      <figure>
+      <figure class="p-2 flex justify-center">
         <img
-        class="h-40 w-full object-cover"
+        class="h-24 w-3/4 object-contain"
         src="${reward.image}"
         alt="${reward.item}"
         >
       </figure>
 
-      <p class="text-lg font-bold">$${reward.cost}</p>
+      <p class="text-lg font-bold text-center mt-auto">$${reward.cost}</p>
     </div>
     `
 
